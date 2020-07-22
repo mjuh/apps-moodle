8ce796ae3a13bdb87c747a66ac68a27a5e23040a
Alexander Rezvov <rezvov@majordomo.ru>
2020-07-22 11:00:44 +0300

    Update to 3.8.4

--- pkgs/moodle/dist.nix
+++ pkgs/moodle/dist.nix
@@ -2,10 +2,10 @@
 
 stdenv.mkDerivation rec {
   pname = "moodle";
-  version = "3.8.3";
+  version = "3.8.4";
   src = fetchurl {
     url = "https://download.moodle.org/stable38/moodle-${version}.tgz";
-    sha256 = "1anjv4gvbb6833j04a1b4aaysnl4h0x96sr1hhm4nm5kq2fimjd1";
+    sha256 = "10glx6ix1z3z1vqsfbps65iqhm35iklkap7fzgms3mk88q8wlnmg";
   };
   patches = [
     ./patches/adminlib-is-dataroot-insecure.patch
