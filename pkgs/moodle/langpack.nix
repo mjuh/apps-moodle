acbe06f1337d0dab748fb5f774d7f675089034da
Oleg Pykhalov <go.wigust@gmail.com>
2021-05-28 15:56:27 +0300

    pkgs: moodle: langpack: Fix sha256.

--- pkgs/moodle/langpack.nix
+++ pkgs/moodle/langpack.nix
@@ -3,6 +3,6 @@
 {
   ru = fetchurl {
     url = "https://download.moodle.org/download.php/direct/langpack/3.8/ru.zip";
-    sha256 = "1bqnmbbhdk6j89nqacch6mk7gz52qfvaiq3pzj2k8iv3y0pr3srx";
+    sha256 = "1mbn0g3hnr589bxc335pyyjlhkdy00ww4ql1zjqly3nla0lvkh24";
   };
 }
