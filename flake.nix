6dc57ac6a46360dbf86d73c4800881afb6a9f796
Зелинский Александр <zelinskiy@majordomo.ru>
2022-04-12 17:12:45 +0300

    return deploy, reformat with nixpkg-fmt

--- flake.nix
+++ flake.nix
@@ -2,26 +2,25 @@
   description = "Docker container with Moodle build by Nix";
 
   inputs = {
-    deploy-rs.url = "github:serokell/deploy-rs";
     flake-compat = { url = "github:edolstra/flake-compat"; flake = false; };
     flake-utils.url = "github:numtide/flake-utils";
     majordomo.url = "git+https://gitlab.intr/_ci/nixpkgs";
     containerImageApache.url = "git+https://gitlab.intr/webservices/apache2-php73.git";
   };
 
-  outputs = { self, flake-utils, nixpkgs, majordomo, deploy-rs,... } @ inputs:
-    flake-utils.lib.eachDefaultSystem (system: {
-      devShell = with nixpkgs.legacyPackages."${system}"; mkShell {
-        buildInputs = [
-          nixUnstable
-          deploy-rs.outputs.packages.${system}.deploy-rs
-        ];
-        shellHook = ''
-          . ${nixUnstable}/share/bash-completion/completions/nix
-          export LANG=C
-        '';
-      };
-    }) // (
+  outputs = { self, flake-utils, nixpkgs, majordomo, ... } @ inputs:
+    flake-utils.lib.eachDefaultSystem
+      (system: {
+        devShell = with nixpkgs.legacyPackages."${system}"; mkShell {
+          buildInputs = [
+            nixUnstable
+          ];
+          shellHook = ''
+            . ${nixUnstable}/share/bash-completion/completions/nix
+            export LANG=C
+          '';
+        };
+      }) // (
       let
         system = "x86_64-linux";
         tests = { driver ? false }: with nixpkgs.legacyPackages.${system}; { } // (with nixpkgs.legacyPackages.${system}.lib;
@@ -37,30 +36,36 @@
               containerImageApache = inputs.containerImageApache.packages.${system}.container-master;
             })));
       in
-        with nixpkgs.legacyPackages.${system}; {
-          packages.${system} = {
-            moodle = (callPackage ./pkgs/moodle { }).dist;
-            moodle-language-pack-ru =
-              callPackage ({ fetchurl }:
+      with nixpkgs.legacyPackages.${system}; {
+        packages.${system} = {
+          moodle = (callPackage ./pkgs/moodle { }).dist;
+          moodle-language-pack-ru =
+            callPackage
+              ({ fetchurl }:
                 fetchurl {
                   url = "https://download.moodle.org/download.php/direct/langpack/3.11/ru.zip";
                   sha256 = "sha256-iOYhPbMQAMsi4g3I8tvTD2msbxK2axKHdT1kYITi35U=";
-                }) {};
-            entrypoint = callPackage ./pkgs/entrypoint {
-              inherit (self.packages.${system}) moodle moodle-language-pack-ru;
-              php = majordomo.packages.${system}.php73;
-            };
-            container = callPackage ./. {
-              inherit (self.packages.${system}) entrypoint;
-              tag = self.packages.${system}.moodle.version;
-            };
-          } // (tests { driver = true; });
-          defaultPackage.${system} = self.packages.${system}.container;
-          checks.${system} = tests { };
-          apps.${system}.vm = {
-            type = "app";
-            program = "${self.packages.${system}.moodle-vm-test-run-moodle-mariadb-nix-upstream}/bin/nixos-run-vms";
+                })
+              { };
+          entrypoint = callPackage ./pkgs/entrypoint {
+            inherit (self.packages.${system}) moodle moodle-language-pack-ru;
+            php = majordomo.packages.${system}.php73;
           };
-        }
+          container = callPackage ./. {
+            inherit (self.packages.${system}) entrypoint;
+            tag = self.packages.${system}.moodle.version;
+          };
+          deploy = majordomo.outputs.deploy {
+            tag = "apps/moodle";
+          };
+        } // (tests { driver = true; });
+        defaultPackage.${system} = self.packages.${system}.container;
+        checks.${system} = tests { };
+        apps.${system}.vm = {
+          type = "app";
+          program = "${self.packages.${system}.moodle-vm-test-run-moodle-mariadb-nix-upstream}/bin/nixos-run-vms";
+        };
+
+      }
     );
 }
